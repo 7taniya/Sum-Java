72473ce47f5491b096c92ffba7dfbd636d05af4f
7taniya <108205147+7taniya@users.noreply.github.com>
2023-01-07 20:25:55 +0530

    Add files via upload

---
+++ javacomp.java
@@ -0,0 +1,16 @@
+public class javacomp {
+    // Sum of the digits
+     public static void main(String[] args) {
+        int k=0;
+int s=0;
+       int n =(int)Math.pow(2, 1000);
+       //System.out.println(n);
+       while(n>0){
+       k=n%10;
+       s=s+k;
+       n=n/10;}
+       System.out.println(s);
+
+    } 
+    
+}
